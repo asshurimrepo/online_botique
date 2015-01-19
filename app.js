0af6ff7a4c0d3d5424e56a9e9c8b4428cf2dd6dc
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-19 18:03:46 +0800

    Updates

--- app.js
+++ app.js
@@ -3,15 +3,51 @@
 
     var app = angular.module("app", []);
 
-    app.controller("BodyController", function($scope) {
+
+
+
+    app.controller("BodyController", function($scope, $http) {
+
+        $scope.config = {tempDir: 'templates/'};
 
         $scope.templates = {
 
-            nav: "templates/nav.html",
-            content: "templates/pages/home.html",
-            spotlight: "templates/spotlight.html"
+            nav: $scope.config.tempDir+"nav.html",
+            content: $scope.config.tempDir+"pages/home.html",
+            spotlight: $scope.config.tempDir+"spotlight.html"
 
         };
+
+
+        $scope.products = [
+            {
+                name: "Mango Man",
+                price: "PHP 300.00",
+                img: "mango-man-8424-764822-1.jpg",
+                new: true
+            },
+
+            {
+                name: "Basic Oxford Sleeve",
+                price: "PHP 699.00",
+                img: "zalora-basics-7865-037002-1.jpg",
+                featured: true
+            },
+
+            {
+                name: "Basic Hoodie",
+                price: "PHP 949.00",
+                img: "zalora-basics-3612-128881-1.jpg",
+                featured: true
+            },
+
+            {
+                name: "Leather Moccasins",
+                price: "PHP 1,699.00",
+                img: "zalora-basics-8437-407371-1.jpg"
+            }
+        ];
+
 
         $scope.init = function(){
             $("#spotlight").owlCarousel({
@@ -21,7 +57,11 @@
 
             new WOW().init();
 
+        };
 
+
+        $scope.changeView = function(view){
+            $scope.templates.content = $scope.config.tempDir+"/pages/"+view+".html";
         };
 
 
